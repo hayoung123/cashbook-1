a3822989c25abf477d9e2d742a19d7c65219e207
edegiil <electropark917@gmail.com>
2021-07-28 10:54:02 +0900

    refactor: 환경변수 dev, prod 분리

--- api/utils/env.ts
+++ api/utils/env.ts
@@ -1,3 +1,6 @@
 import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({
+  path: path.resolve(process.cwd(), process.env.NODE_ENV === 'development' ? '.env.dev' : '.env'),
+});
