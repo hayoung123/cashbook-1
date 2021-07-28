de4b0266e594138c8a5988578753efc17d10ac59
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-28 15:13:15 +0900

    feat. 컴포넌트 프레임워크 구현 (#25)
    - HTMLElement를 상속받아서 this로 Element 접근하게 구현
    - state초기화해주는 initState
    - render에 필요한 setTemplate,setLayout,setComponent 구현
    - 클래스 추가를 위한 addClass 구현

--- client/src/lib/component/index.ts
+++ client/src/lib/component/index.ts
@@ -1,64 +1,79 @@
+import { subscribe, unsubscribe } from '../observer';
 type objType = {
   [key: string]: any;
 };
 
-type ComponentType = {
-  props: any;
-  state: any;
-  keys: Array<string>;
-  reRender: () => void;
-  components: objType;
-};
-
-export default class Component {
-  public props: any;
+export default class Component extends HTMLElement {
+  public props?: any;
   public state: any;
   public keys: Array<string>;
   public reRender: () => void;
   public components: objType;
 
-  constructor(props: any) {
+  constructor(props?: any) {
+    super();
     this.props = props;
-    this.state;
+    this.state = {};
 
     this.keys = [];
-    this.components;
+    this.components = {};
+
     this.init();
     this.reRender = this.subscribedRender.bind(this);
   }
+
   init() {
+    this.state = this.initState();
     this.render();
     this.addEvent();
   }
-  render() {
-    // this.components = this.setComponents();
-    // this.innerHTML = setTemplate();
+
+  initState() {
+    return {};
   }
+
   addEvent() {}
 
-  //   setTemplate() {
-  //     return `
-  //       <div>1</div>
-  //       <Component>
-  //       <div>3</div>
-  //     `;
-  //   }
+  render() {
+    this.components = this.setComponents();
+    this.innerHTML = this.setTemplate();
+    this.setLayout();
+  }
 
-  //   appendChild () {
-  //     const $button = query('#page__button')
-  //     $button.append(new Button())
-  //     $root.replaceChild(new Button(), $button)
-  //   }
+  //innerHTML
+  setTemplate(): string {
+    return '';
+  }
 
-  setComponents() {}
+  //컴포넌트를 각 위치에 맞게 replace
+  setLayout() {
+    for (const [key, Comp] of Object.entries(this.components)) {
+      const $$ = this.querySelector(`#${key}`) as HTMLElement;
+      this.replaceChild(Comp, $$);
+    }
+  }
+
+  //사용하는 컴포넌트 init
+  setComponents() {
+    return {};
+  }
+
+  //클래스 추가 메소드
+  addClass(...args: Array<string>) {
+    this.className = args.join(' ');
+  }
+
   //구독한 상태 변경시 렌더링 되는 함수
   subscribedRender() {
     this.unsubscribe(); //하위 컴포넌트 구독 해제
     this.render(); //하위 컴포넌트 재생성
   }
+
+  //key에 속한 것들 render
   subscribe() {
-    this.keys.forEach((key) => subscribe(key, this.render));
+    this.keys.forEach((key) => subscribe(key, this.reRender));
   }
+
   unsubscribe(isCurrentComp = true) {
     if (!isCurrentComp && this.keys.length) {
       this.keys.forEach((key) => unsubscribe(key, this.reRender));
