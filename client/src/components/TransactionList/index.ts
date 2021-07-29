893a837e29673c4b346b298eaaf247e7408029a7
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-29 10:16:39 +0900

    markup: 거래내역 목데이터 및 뼈대 구현 (#26)

---
+++ client/src/components/TransactionList/index.ts
@@ -0,0 +1,78 @@
+import { objType } from 'src/type/type';
+import Component from 'src/lib/component';
+import DayTransaction from './DayTransaction';
+
+type PaymentType = {
+  category: string;
+  title: string;
+  method: string;
+  price: number;
+};
+
+export default class TransationList extends Component {
+  constructor() {
+    super();
+  }
+
+  //TODO 진짜데이터 props로 받아와서 처리
+  setTemplate(): string {
+    const template = sampleData.reduce((acc, cur, idx) => {
+      return acc + `<div id='transaction-${idx}'></div>`;
+    }, '');
+    return template;
+  }
+
+  //TODO 진짜데이터 props로 받아와서 처리
+  setComponents(): { [key: string]: HTMLElement } {
+    const components: objType = {};
+    sampleData.forEach((data, idx) => {
+      const key = `transaction-${idx}`;
+      components[key] = new DayTransaction({ ...data });
+    });
+    return components;
+  }
+}
+
+customElements.define('transaction-list', TransationList);
+
+type DayTransactionType = {
+  date: string;
+  transaction: Array<PaymentType>;
+};
+
+const sampleData: Array<DayTransactionType> = [
+  {
+    date: '2021.07.28',
+    transaction: [
+      {
+        category: '문화/여가',
+        title: '스트리밍서비스 정기 결제',
+        method: '현대카드',
+        price: -10900,
+      },
+      {
+        category: '교통',
+        title: '후불 교통비 결제',
+        method: '현대카드',
+        price: -45340,
+      },
+    ],
+  },
+  {
+    date: '2021.07.27',
+    transaction: [
+      {
+        category: '미분류',
+        title: '온라인 세미나 신청',
+        method: '현대카드',
+        price: -10000,
+      },
+      {
+        category: '월급',
+        title: '7월 급여',
+        method: '현금',
+        price: 1000000,
+      },
+    ],
+  },
+];
