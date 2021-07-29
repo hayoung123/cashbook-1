f2517c344ab38d33ecf9dda9035e478b21c28a97
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-29 18:50:46 +0900

    markup: 수정모드 마크업 (#9)

--- client/src/components/TransactionForm/index.ts
+++ client/src/components/TransactionForm/index.ts
@@ -2,38 +2,50 @@ import Component from 'src/lib/component';
 
 import inActiveSubmitBtn from 'public/assets/icon/inActiveSubmitBtn.svg';
 import activeSubmitBtn from 'public/assets/icon/activeSubmitBtn.svg';
+import downArrow from 'public/assets/icon/downArrow.svg';
+import trashIcon from 'public/assets/icon/trashIcon.svg';
 
 import './style.scss';
 import _ from 'src/utils/dom';
 
-type FormType = {
+interface FormType {
   date: string;
   category: string;
   title: string;
   method: string;
   price: number;
-};
+}
 
-type StateType = {
+interface PropsType {
+  isEdit: boolean;
+  data: FormType;
+}
+
+interface StateType {
   isIncome: boolean;
-};
+  isAbleSubmit: boolean;
+}
 
 const INIT_FORM = {
-  date: '',
-  category: '',
-  title: '',
-  method: '',
-  price: 0,
+  isEdit: false,
+  data: {
+    date: '',
+    category: '',
+    title: '',
+    method: '',
+    price: 0,
+  },
 };
 
-export default class TransactionFrom extends Component<StateType, FormType> {
-  constructor(props: FormType = INIT_FORM) {
+export default class TransactionFrom extends Component<StateType, PropsType> {
+  constructor(props: PropsType = INIT_FORM) {
     super(props);
     this.addClass('transaction__form-container');
   }
   initState(): StateType {
     return {
       isIncome: true,
+      isAbleSubmit: false,
     };
   }
 
@@ -41,15 +53,24 @@ export default class TransactionFrom extends Component<StateType, FormType> {
     _.onEvent(this, 'click', this.handleClick.bind(this));
   }
   setTemplate(): string {
-    if (!this.props || !this.state) return '';
+    if (!this.state) return '';
 
-    const { date, category, title, method, price } = this.props;
-    const { isIncome } = this.state;
+    const { date, category, title, method, price } = this.props.data;
+    const { isIncome, isAbleSubmit } = this.state;
 
     return `
     <div class="transaction__type">
-      <button class='transaction__type-btn${isIncome ? ' selected' : ''}'>수입</button>
-      <button class='transaction__type-btn${isIncome ? '' : ' selected'}'>지출</button>
+      <div class='transaction__type-btns'>
+        <button class='transaction__type-btn${isIncome ? ' selected' : ''}'>수입</button>
+        <button class='transaction__type-btn${isIncome ? '' : ' selected'}'>지출</button>
+      </div>
+      ${
+        this.props.isEdit
+          ? `<button class='transaction__delete-btn'>
+        <img src=${trashIcon} alt='삭제하기 버튼' />
+      </button>`
+          : ''
+      }
     </div>
     <div class="transaction__form">
       <div class="transaction__form-column transaction__date" >
@@ -58,7 +79,10 @@ export default class TransactionFrom extends Component<StateType, FormType> {
       </div>
       <div class="transaction__form-column transaction__category" >
         <div>분류</div>
-        <div class="${category ? '' : 'empty'}">${category ? category : '선택하세요'}</div>
+        <div>
+          <div class="${category ? '' : 'empty'}">${category ? category : '선택하세요'}</div>
+          <img class="form-dropdown-btn" src=${downArrow} alt='펼쳐보기' />
+        </div>
       </div>
       <div class="transaction__form-column transaction__title" >
         <label for='title'>내용</label>
@@ -66,7 +90,10 @@ export default class TransactionFrom extends Component<StateType, FormType> {
       </div>
       <div class="transaction__form-column transaction__method" >
         <div>결제수단</div>
-        <div class="${method ? '' : 'empty'}">${method ? method : '선택하세요'}</div>
+        <div>
+          <div class="${method ? '' : 'empty'}">${method ? method : '선택하세요'}</div>
+          <img class="form-dropdown-btn" src=${downArrow} alt='펼쳐보기' />
+        </div>
       </div>
       <div class="transaction__form-column transaction__price" >
         <label for='price'>금액</label>
@@ -76,7 +103,7 @@ export default class TransactionFrom extends Component<StateType, FormType> {
         </div>
       </div>
       <div class="transaction__form-submit-btn">
-        <img src=${inActiveSubmitBtn} alt='제출 버튼' />
+        <img src=${isAbleSubmit ? activeSubmitBtn : inActiveSubmitBtn} alt='제출 버튼' />
       </div>
     </div>
     `;
@@ -84,20 +111,32 @@ export default class TransactionFrom extends Component<StateType, FormType> {
 
   handleClick(e: Event): void {
     const target = e.target as HTMLElement;
+    //제출 버튼
     if (this.isSubmitBtn(target)) {
       //서버요청
       return;
     }
+
+    //수입,지출 버튼
     if (this.isTransactionTypeBtn(target)) {
       const isIncome = target.textContent === '수입';
       this.setState({ isIncome });
     }
+
+    //드롭다운 버튼
+    if (this.isDropdownBtn(target)) {
+      //category, method dropdown 관련 버튼
+    }
+  }
+
+  isSubmitBtn(target: HTMLElement): boolean {
+    return !!target.closest('.transaction__form-submit-btn');
   }
   isTransactionTypeBtn(target: HTMLElement): boolean {
     return !!target.closest('.transaction__type-btn');
   }
-  isSubmitBtn(target: HTMLElement): boolean {
-    return !!target.closest('.transaction__form-submit-btn');
+  isDropdownBtn(target: HTMLElement): boolean {
+    return !!target.closest('.form-dropdown-btn');
   }
 }
 
