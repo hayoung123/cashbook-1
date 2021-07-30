2dfad731f48eaa5f006fa2fa5a5076e7c4589776
hayoung <hayoung@adminui-MacBookPro.local>
2021-07-30 11:01:42 +0900

    feat: 전역 로그인 관리 상태 추가 (#26)

--- client/src/store/page.ts
+++ client/src/store/page.ts
@@ -5,3 +5,8 @@ export const pageState = initState({
   key: 'pageState',
   defaultValue: { Page: MainPage },
 });
+
+export const isLoggedInState = initState({
+  key: '로그인 상태',
+  defaultValue: false,
+});
