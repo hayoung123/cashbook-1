cc819543e2246c142c0b780ba4962bf950216a49
hayoung123 <proshy1@naver.com>
2021-07-31 19:46:20 +0900

    feat: 결재수단 중복 및 이미 소유하고 있는 결제 에러 타입 추가  (#16)

--- api/utils/errorHandler.ts
+++ api/utils/errorHandler.ts
@@ -13,6 +13,10 @@ function errorHandler(errCode: string): ErrorType {
       return { statusCode: 401, errorMessage: '유효하지 않은 토큰입니다.' };
     case 'auth/existing-email':
       return { statusCode: 409, errorMessage: '이미 가입된 이메일입니다.' };
+    case 'payment/nonexistent-payment':
+      return { statusCode: 409, errorMessage: '이미 가입된 이메일입니다.' };
+    case 'payment/owned-payment':
+      return { statusCode: 409, errorMessage: '이미 가입된 이메일입니다.' };
     default:
       return { statusCode: 500, errorMessage: '다시 시도해주세요.' };
   }
