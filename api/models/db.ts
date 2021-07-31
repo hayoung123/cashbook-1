b99ffc2dff9bf971d8e46440e73b74f69ed6058c
edegiil <electropark917@gmail.com>
2021-07-31 11:36:53 +0900

    refactor: DB 불러오기 로직 변경(#5)

--- api/models/db.ts
+++ api/models/db.ts
@@ -1,4 +1,13 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, ModelCtor, Model } from 'sequelize';
+// import { User, Transaction, Payment, USER_has_PAYMENT } from './schema';
+import schema from './schema';
+
+interface ModelType {
+  User: ModelCtor<Model<any, any>>;
+  Transaction: ModelCtor<Model<any, any>>;
+  Payment: ModelCtor<Model<any, any>>;
+  USER_has_PAYMENT: ModelCtor<Model<any, any>>;
+}
 
 const DB_NAME = process.env.DB_NAME || '';
 const DB_HOST = process.env.DB_HOST || '';
@@ -9,5 +18,12 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'mysql',
 });
+
+export const db: ModelType = {
+  User: schema(sequelize).User,
+  Transaction: schema(sequelize).Transaction,
+  Payment: schema(sequelize).Payment,
+  USER_has_PAYMENT: schema(sequelize).USER_has_PAYMENT,
+};
 
 export default sequelize;
