dae0e70af2a7b4033a5666619081dfa28644bd75
edegiil <electropark917@gmail.com>
2021-07-31 11:37:37 +0900

    feat: 회원가입 API

--- api/services/auth.ts
+++ api/services/auth.ts
@@ -1,11 +1,62 @@
-export function SignUp() {}
+import sequelize, { db } from 'models/db';
+import errorGenerator from 'utils/errorGenerator';
+import { createToken } from 'utils/jwt';
+import hashPassword from 'utils/hashPassword';
 
-export function SignIn() {}
+interface SignUpType {
+  accessToken: string;
+  refreshToken: string;
+}
 
-export function SignOut() {}
+async function signUp(email: string, password: string): Promise<SignUpType> {
+  const userCount = await db.User.count({
+    where: {
+      email,
+    },
+  });
+
+  if (userCount > 0) {
+    throw errorGenerator({
+      message: 'Existing email account',
+      code: 'auth/existing-email',
+    });
+  }
+
+  const transaction = await sequelize.transaction();
+
+  const hashedPassword = await hashPassword(password);
+
+  const user = await db.User.create(
+    {
+      email,
+      password: hashedPassword,
+      is_OAuth: false,
+    },
+    { transaction },
+  );
+
+  const uid = user.getDataValue('id');
+
+  const accessToken = createToken('access', { uid });
+  const refreshToken = createToken('refresh', { uid });
+
+  await db.User.update(
+    {
+      refresh_token: refreshToken,
+    },
+    {
+      where: {
+        id: uid,
+      },
+      transaction,
+    },
+  );
+
+  await transaction.commit();
+
+  return { accessToken, refreshToken };
+}
 
 export default {
-  SignIn,
-  SignUp,
-  SignOut,
+  signUp,
 };
