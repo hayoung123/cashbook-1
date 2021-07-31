43317d7f6e326389a46eebe720beefb43ad27b9e
hayoung123 <proshy1@naver.com>
2021-07-31 20:56:17 +0900

    feat: payment 라우터 등록 (#16)

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import transaction from './transaction';
 import auth from './auth';
+import transaction from './transaction';
+import payment from './payment';
 
 const router = express.Router();
 
 router.use('/transaction', transaction);
 router.use('/auth', auth);
+router.use('/payment', payment);
 
 export default router;
