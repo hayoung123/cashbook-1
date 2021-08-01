105c16728669503e60084862bcfe1f60af4fbbff
edegiil <electropark917@gmail.com>
2021-08-01 19:58:55 +0900

    feat: 회원가입 페이지 API 연동(#88)

--- client/src/pages/SignUpPage/index.ts
+++ client/src/pages/SignUpPage/index.ts
@@ -1,8 +1,18 @@
 import Component from 'src/lib/component';
 
 import { router } from '../../../index';
+import fetchWrapper from 'src/utils/fetchWrapper';
+import checkInputValidity from 'src/utils/checkInputValidity';
+
+import { SIGNUP_URL } from 'src/configs/urls';
 
 import './style.scss';
+
+interface FormType extends EventTarget {
+  email?: HTMLInputElement;
+  password?: HTMLInputElement;
+  'password-check'?: HTMLInputElement;
+}
 
 export default class SignUpPage extends Component {
   constructor() {
@@ -46,9 +56,65 @@ export default class SignUpPage extends Component {
     }
   }
 
-  handleSubmit(e: Event): void {
-    e.preventDefault();
-    console.log('submit!');
+  async handleSubmit(e: Event): Promise<void> {
+    try {
+      e.preventDefault();
+      if (!e.target) {
+        return;
+      }
+
+      const $form: FormType = e.target;
+
+      const email = $form.email?.value;
+      const password = $form.password?.value;
+      const passwordCheck = $form['password-check']?.value;
+
+      if (!email) {
+        alert('이메일을 입력해주세요.');
+        return;
+      }
+
+      const isEmailValid = checkInputValidity('email', email);
+      if (!isEmailValid) {
+        alert('이메일 형식을 확인해주세요');
+        return;
+      }
+
+      if (!password) {
+        alert('비밀번호를 입력해주세요.');
+        return;
+      }
+
+      const isPasswordValid = checkInputValidity('password', password);
+      if (!isPasswordValid) {
+        alert('비밀번호 형식이 다릅니다.');
+        return;
+      }
+
+      if (password !== passwordCheck) {
+        alert('비밀번호와 비밀번호 확인이 서로 다릅니다.');
+        return;
+      }
+
+      const res = await fetchWrapper(SIGNUP_URL, 'POST', {
+        email,
+        password,
+      });
+
+      if (!res.success) {
+        alert(res.errorMessage);
+        return;
+      }
+
+      const { accessToken } = res;
+
+      localStorage.setItem('_at', accessToken);
+
+      // TODO: 라우터에서 isLoggedIn을 true로 만들어줘야 함
+      router.replace('/');
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
