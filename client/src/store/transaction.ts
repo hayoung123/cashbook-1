3e0344ded05c1f692f6ce3b77c2222310c23cc98
hayoung123 <proshy1@naver.com>
2021-08-01 20:24:59 +0900

    feat: 거래내역 타입 상태 수입/지출 상태 생성 (#28)

--- client/src/store/transaction.ts
+++ client/src/store/transaction.ts
@@ -4,3 +4,8 @@ export const dateState = initState({
   key: 'dateState',
   defaultValue: { year: new Date().getFullYear(), month: new Date().getMonth() + 1 },
 });
+
+export const transactionTypeState = initState({
+  key: 'transactionTypeState-isIncome/isExpenditure',
+  defaultValue: { isIncome: true, isExpenditure: true },
+});
