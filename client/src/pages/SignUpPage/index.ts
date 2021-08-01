69542b7b0be2ca23af06299c4248e17c8faaefad
edegiil <electropark917@gmail.com>
2021-08-01 23:22:28 +0900

    refactor: 회원가입 페이지 input 검사 방식 변경(#88)

--- client/src/pages/SignUpPage/index.ts
+++ client/src/pages/SignUpPage/index.ts
@@ -1,8 +1,10 @@
 import Component from 'src/lib/component';
 
 import { router } from '../../../index';
+import { setState } from 'src/lib/observer';
+import { isLoggedInState } from 'src/store/page';
 import fetchWrapper from 'src/utils/fetchWrapper';
-import checkInputValidity from 'src/utils/checkInputValidity';
+import getValidityMessage from 'src/utils/getValidityMessages';
 
 import { SIGNUP_URL } from 'src/configs/urls';
 
@@ -27,9 +29,10 @@ export default class SignUpPage extends Component {
           <h3 class="logo">우아한 가계부</h3>
           <h1 class="title">회원가입</h1>
           <form>
-            <input id="email" placeholder="이메일" type="email" />
-            <input id="password" placeholder="비밀번호" type="password" />
+            <input id="email" placeholder="이메일" type="email" required />
+            <input id="password" placeholder="비밀번호" type="password" minlength="5" required />
             <input id="password-check" placeholder="비밀번호 확인" type="password" />
+            <span class="error-message" id="error-message"></span>
             <button type="submit">회원가입</button>
           </form>
           <div class="sub-buttons">
@@ -44,16 +47,45 @@ export default class SignUpPage extends Component {
   addEvent(): void {
     this.addEventListener('click', this.handleButtonClick.bind(this));
     this.addEventListener('submit', this.handleSubmit.bind(this));
+    this.querySelector('#email')?.addEventListener('invalid', this.handleInputError.bind(this));
+    this.querySelector('#password')?.addEventListener('invalid', this.handleInputError.bind(this));
+    this.querySelector('#password-check')?.addEventListener(
+      'invalid',
+      this.handleInputError.bind(this),
+    );
   }
 
   handleButtonClick(e: Event): void {
     const target = e.target as HTMLElement;
     const button: HTMLButtonElement | null = target.closest('button');
     if (!button) return;
-    console.log(button);
+
     if (button.id === 'signin') {
       router.push('/');
     }
+  }
+
+  displayError(message: string): void {
+    const $errorMessage = this.querySelector(`#error-message`);
+    if (!$errorMessage) {
+      return;
+    }
+
+    $errorMessage.textContent = message;
+  }
+
+  handleInputError(e: Event): void {
+    e.preventDefault();
+
+    const $input: HTMLInputElement = e.target as HTMLInputElement;
+
+    const type = $input.id === 'email' ? 'email' : 'password';
+    const errorMessage = getValidityMessage(type, $input.validity);
+
+    if (!errorMessage) {
+      return;
+    }
+    this.displayError(errorMessage);
   }
 
   async handleSubmit(e: Event): Promise<void> {
@@ -69,30 +101,8 @@ export default class SignUpPage extends Component {
       const password = $form.password?.value;
       const passwordCheck = $form['password-check']?.value;
 
-      if (!email) {
-        alert('이메일을 입력해주세요.');
-        return;
-      }
-
-      const isEmailValid = checkInputValidity('email', email);
-      if (!isEmailValid) {
-        alert('이메일 형식을 확인해주세요');
-        return;
-      }
-
-      if (!password) {
-        alert('비밀번호를 입력해주세요.');
-        return;
-      }
-
-      const isPasswordValid = checkInputValidity('password', password);
-      if (!isPasswordValid) {
-        alert('비밀번호 형식이 다릅니다.');
-        return;
-      }
-
       if (password !== passwordCheck) {
-        alert('비밀번호와 비밀번호 확인이 서로 다릅니다.');
+        this.displayError('비밀번호와 비밀번호 확인이 서로 다릅니다.');
         return;
       }
 
@@ -110,7 +120,7 @@ export default class SignUpPage extends Component {
 
       localStorage.setItem('_at', accessToken);
 
-      // TODO: 라우터에서 isLoggedIn을 true로 만들어줘야 함
+      setState(isLoggedInState)(true);
       router.replace('/');
     } catch (err) {
       console.log(err);
