46a689ebf83622aba2ccbeea69956ca67e374055
edegiil <electropark917@gmail.com>
2021-08-01 14:40:49 +0900

    feat: 차트 페이지에 필요한 통계 데이터 API(#78)

--- api/services/transaction.ts
+++ api/services/transaction.ts
@@ -1,3 +1,5 @@
+import { Op } from 'sequelize';
+
 import { db } from 'models/db';
 import errorGenerator from 'utils/errorGenerator';
 
@@ -15,6 +17,12 @@ interface TransactionDataType {
 interface EditTransactionDataType extends TransactionDataType {
   transactionId: string;
 }
+
+type Category = 'life' | 'food' | 'transport' | 'shop' | 'health' | 'culture' | 'etc';
+
+type C = {
+  [key in Category]: number;
+};
 
 //거래내역 추가
 async function createTransaction({
@@ -130,4 +138,91 @@ async function checkUserTransaction(userId: string, transactionId: string): Prom
   return !!isUserTransaction;
 }
 
-export default { createTransaction, deleteTransaction, editTransaction };
+async function getStatistics(
+  uid: string,
+  type: string,
+  year: string,
+  month: string,
+  category: string,
+): Promise<C | any[] | void> {
+  if (type === 'category') {
+    if (!year || !month) {
+      throw errorGenerator({
+        code: 'req/query-not-found',
+        message: 'Required query not found',
+      });
+    }
+    const currentMonthStart = `${year}-${month}`;
+    const currentMonthEnd = `${year}-${+month + 1}`;
+
+    const transactionSnapshot = await db.Transaction.findAll({
+      attributes: ['category', 'price'],
+      where: {
+        USERId: uid,
+        // TODO: 수입 지출 구분방법에 따라 달라져야 함
+        // price: {
+        //   [Op.lte]: 0,
+        // },
+        date: {
+          [Op.lt]: new Date(currentMonthEnd),
+          [Op.gte]: new Date(currentMonthStart),
+        },
+      },
+    });
+
+    const categoryStatistics: C = {
+      life: 0,
+      food: 0,
+      transport: 0,
+      shop: 0,
+      health: 0,
+      culture: 0,
+      etc: 0,
+    };
+
+    transactionSnapshot.forEach((t) => {
+      const category: Category = t.getDataValue('category');
+      const price: number = +t.getDataValue('price');
+
+      categoryStatistics[category] += price;
+    });
+
+    return categoryStatistics;
+  }
+
+  if (type === 'trend') {
+    if (!year || !category) {
+      throw errorGenerator({
+        code: 'req/query-not-found',
+        message: 'Required query not found',
+      });
+    }
+
+    const transactionSnapshot = await db.Transaction.findAll({
+      attributes: ['date', 'price'],
+      where: {
+        USERId: uid,
+        category,
+        date: {
+          [Op.lte]: new Date(`${year}-12-31`),
+          [Op.gte]: new Date(`${year}-1-1`),
+        },
+      },
+    });
+
+    const trendStatistics = new Array(13).fill(0);
+
+    transactionSnapshot.forEach((t) => {
+      const price = t.getDataValue('price');
+      const date = t.getDataValue('date');
+      const month = new Date(date).getMonth();
+
+      trendStatistics[month] += +price;
+    });
+
+    return trendStatistics;
+  }
+  return;
+}
+
+export default { createTransaction, deleteTransaction, editTransaction, getStatistics };
