cdd59d5795096179295f702d3207075aa0af6385
edegiil <electropark917@gmail.com>
2021-08-02 19:54:29 +0900

    feat: 선 차트(#34)

--- client/src/pages/ChartPage/drawCoordinatePlane.ts
+++ client/src/pages/ChartPage/drawCoordinatePlane.ts
@@ -1,3 +1,8 @@
+export const COORDINATE_WIDTH = 750;
+export const COORDINATE_HEIGHT = 300;
+export const COLUMNS = 13;
+export const ROWS = 10;
+
 function drawCoordinatePlane(element: HTMLElement, canvasID: string): void {
   const canvas: HTMLCanvasElement | null = element.querySelector(`#${canvasID}`);
 
@@ -11,25 +16,23 @@ function drawCoordinatePlane(element: HTMLElement, canvasID: string): void {
     return;
   }
 
-  const coordinateWidth = 750;
-  const coordinateHeight = 300;
-  const gridHeight = 25;
-  const gridWidth = 32.6;
+  const gridHeight = COORDINATE_HEIGHT / ROWS;
+  const gridWidth = COORDINATE_WIDTH / COLUMNS;
 
-  for (let i = 0; i < 13; i++) {
+  for (let i = 0; i < COLUMNS; i++) {
     ctx.strokeStyle = '#ccd3d3';
-    ctx?.beginPath();
-    ctx?.moveTo(0, gridHeight * i);
-    ctx?.lineTo(coordinateWidth, gridHeight * i);
-    ctx?.stroke();
+    ctx.beginPath();
+    ctx.moveTo(0, gridHeight * i);
+    ctx.lineTo(COORDINATE_WIDTH, gridHeight * i);
+    ctx.stroke();
   }
 
-  for (let i = 0; i < 24; i++) {
+  for (let i = 0; i < COLUMNS; i++) {
     ctx.strokeStyle = '#ccd3d3';
-    ctx?.beginPath();
-    ctx?.moveTo(gridWidth * i, 0);
-    ctx?.lineTo(gridWidth * i, coordinateHeight);
-    ctx?.stroke();
+    ctx.beginPath();
+    ctx.moveTo(gridWidth * i, 0);
+    ctx.lineTo(gridWidth * i, COORDINATE_HEIGHT);
+    ctx.stroke();
   }
 }
 
