d316bb26d907017ae653265369170086e46ac296
hayoung123 <proshy1@naver.com>
2021-08-02 23:51:36 +0900

    feat: 카테고리 키 가져오는 유틸함수 (#28)

---
+++ client/src/utils/category.ts
@@ -0,0 +1,8 @@
+import { CATEGORY__INFO } from 'src/constant/category';
+
+export const getCategoryKey = (category: string): string => {
+  for (const [key, { name }] of Object.entries(CATEGORY__INFO)) {
+    if (category === name) return key;
+  }
+  return '';
+};
