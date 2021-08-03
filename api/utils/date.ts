b473c58954ce8c09bab054ce8ff7341e0443b2fa
hayoung123 <proshy1@naver.com>
2021-08-03 16:40:04 +0900

    refactor: 월초, 말 구하는 함수, 거래내역으로 파싱하는 함수 유틸로 파일분리 (#97)

---
+++ api/utils/date.ts
@@ -0,0 +1,35 @@
+import { DayTransactionType, TransactionRecordType } from './../types/transaction';
+
+//날짜 시작한날 끝날 구하기
+export function getSideDate(year: number, month: number): { startDate: Date; endDate: Date } {
+  const lastDate = new Date(year, month, 0).getDate();
+  return {
+    startDate: new Date(year, month - 1, 1),
+    endDate: new Date(year, month - 1, lastDate),
+  };
+}
+
+//거래내역 파싱
+export const parseTransactionByDate = (
+  transactions: Array<TransactionRecordType>,
+): Array<DayTransactionType> => {
+  const result: Array<DayTransactionType> = [];
+  let dayRecord: DayTransactionType = { date: '', transaction: [] };
+
+  transactions.forEach((record) => {
+    if (dayRecord.date === record.date) {
+      dayRecord.transaction?.push(record);
+      return;
+    }
+    if (dayRecord.date) {
+      result.push(dayRecord);
+      dayRecord = { date: '', transaction: [] };
+    }
+    dayRecord.date = record.date;
+    dayRecord.transaction = [record];
+  });
+
+  if (dayRecord.date) result.push(dayRecord);
+
+  return result;
+};
