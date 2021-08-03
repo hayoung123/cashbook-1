48f83e403c6e835813a51c8e7d53f3d8ac2afaaa
hayoung123 <proshy1@naver.com>
2021-08-03 16:41:18 +0900

    refactor: 통계 라우터 타입별로 서비스 구분 (#97)

--- api/routes/transaction/statistics/index.ts
+++ api/routes/transaction/statistics/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 
-import transactionService from 'services/transaction';
+import transactionService from 'services/transaction-statistics';
 
 import { decodeToken, getAccessToken } from 'utils/jwt';
 import errorGenerator from 'utils/error-generator';
 import errorHandler from 'utils/error-handler';
+
+import { CategoryType } from 'types/common';
+import { CategoryStatisticsType, CalendarStatisticsType } from 'types/statistics';
 
 const router = express.Router();
 
@@ -29,13 +32,23 @@ router.get('/', async (req, res) => {
     const token = getAccessToken(req.headers.authorization);
     const { uid } = decodeToken(token);
 
-    const result = await transactionService.getStatistics(
-      uid,
-      type,
-      `${year}`,
-      `${month}`,
-      `${category}`,
-    );
+    let result: CategoryStatisticsType | number[] | void | CalendarStatisticsType;
+
+    if (type === 'category') {
+      result = await transactionService.getCategoryStatistics(uid, year as string, month as string);
+    }
+
+    if (type === 'trend') {
+      result = await transactionService.getTrendStatistics(
+        uid,
+        year as string,
+        category as CategoryType,
+      );
+    }
+
+    if (type === 'calendar') {
+      result = await transactionService.getCalendarStatistics(uid, year as string, month as string);
+    }
 
     res.status(200).json({ result });
   } catch (err) {
