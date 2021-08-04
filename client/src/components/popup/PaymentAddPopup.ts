e14e1168cc51d6de17080e28918358e1075cafa8
hayoung123 <proshy1@naver.com>
2021-08-04 17:05:40 +0900

    markup: 결제수단 버튼 위치 수정  (#31)

--- client/src/components/popup/PaymentAddPopup.ts
+++ client/src/components/popup/PaymentAddPopup.ts
@@ -8,7 +8,7 @@ export default class PaymentAddPupup extends Component<any, void> {
         <div class='popup'>
           <div class='popup__tiitle'>추가하실 결제수단을 적어주세요.</div>
           <input placeholder="입력하세요"></input>
-          <div class='popup__error'>hello</div>
+          <div class='popup__error'></div>
           <div class='popup__btns'>
             <div class='popup__cancle-btn'>취소</div>
             <div class='popup__submit-btn'>등록</div>
