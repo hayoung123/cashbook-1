1a70af29ff44c595d1ae920737c44ffe2d196d98
edegiil <electropark917@gmail.com>
2021-08-05 20:19:41 +0900

    refactor: cors 설정 변경

--- api/configs/cors.ts
+++ api/configs/cors.ts
@@ -1,6 +1,5 @@
 const corsOptions = {
-  // origin: 'http://localhost:8000', // 접근 권한을 부여하는 도메인 (Web Server)
-  origin: 'http://3.34.139.247:3000', // 접근 권한을 부여하는 도메인 (Web Server)
+  origin: process.env.CLIENT_ORIGIN, // 접근 권한을 부여하는 도메인 (Web Server)
   credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
   optionsSuccessStatus: 200, // 응답 상태 200으로 설정
 };
