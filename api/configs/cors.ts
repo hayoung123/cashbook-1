e91b7639319cfd4369e7c9dfe3b4bd856ab6f1ea
hayoung123 <proshy1@naver.com>
2021-08-05 18:17:23 +0900

    fix: cors origin

--- api/configs/cors.ts
+++ api/configs/cors.ts
@@ -1,5 +1,6 @@
 const corsOptions = {
-  origin: 'http://localhost:8000', // 접근 권한을 부여하는 도메인 (Web Server)
+  // origin: 'http://localhost:8000', // 접근 권한을 부여하는 도메인 (Web Server)
+  origin: 'http://3.34.139.247:3000', // 접근 권한을 부여하는 도메인 (Web Server)
   credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
   optionsSuccessStatus: 200, // 응답 상태 200으로 설정
 };
